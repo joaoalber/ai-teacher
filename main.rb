e129614de71600538baacbc5548715dafd114752
João Rossi <30991248+joaoalber@users.noreply.github.com>
2024-01-20 15:03:29 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,72 +6,64 @@ require 'uri'
 require 'json'
 require 'oj'
 
-url = URI('https://api.openai.com/v1/audio/transcriptions')
-token = 'token'
+BASE_URL = 'https://api.openai.com/v1'.freeze
+FILE_PATH = 'your_file_path' # example => C:/Users/spwnd/Desktop/Recording0010.mp3'
+TOKEN = 'token'
 
-file_path = 'C:/Users/spwnd/Desktop/Recording0009.mp3'
-file = UploadIO.new(file_path, 'audio/mpeg')
+def post(url, body = nil, multipart: false)
+  if multipart
+    file = UploadIO.new(FILE_PATH, 'audio/mpeg')
+    request = Net::HTTP::Post::Multipart.new(url.path, 'file' => file, 'model' => 'whisper-1', 'response_format' => 'text', 'language' => 'en')
+    request['Authorization'] = "Bearer #{TOKEN}"
 
-request = Net::HTTP::Post::Multipart.new url.path,
-  'file' => file,
-  'model' => 'whisper-1',
-  'response_format' => 'text',
-  'language' => 'en'
+    Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(request) } 
+  else
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    headers = { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{TOKEN}" }
 
-request['Authorization'] = "Bearer #{token}"
-
-response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
-  http.request(request)
+    http.post(url, body, headers)
+  end
 end
 
-url = URI.parse('https://api.openai.com/v1/chat/completions')
+def transcription
+  url = URI(BASE_URL + '/audio/transcriptions')
 
-body = {
-  model: "gpt-3.5-turbo-1106",
-  messages: [
-    {
-      role: "user",
-      content: "You are a helpful assistant designed to talk English with me, make questions, act as a normal person."
-    },
-    {
-      role: "user",
-      content: "#{response.body}"
-    }
-  ]
-}.to_json
-
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-headers = {
-  'Content-Type' => 'application/json',
-  'Authorization' => "Bearer #{token}"
-}
-
-response = http.post(url, body, headers)
-
-response = Oj.load(response.body)
-
-url = URI.parse('https://api.openai.com/v1/audio/speech')
-
-# Create the request body
-body = {
-  model: "tts-1",
-  input: response["choices"][0]["message"]["content"],
-  voice: "alloy"
-}.to_json
-
-# Configure the HTTP request
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-headers = {
-  'Content-Type' => 'application/json',
-  'Authorization' => "Bearer #{token}"
-}
-
-# Perform the POST request
-response = http.post(url, body, headers)
-
-# Save the response body to a file
-File.open('speech.mp3', 'wb') do |file|
-  file.write(response.body)
+  post(url, multipart: true)
 end
+
+def chat
+  url = URI.parse(BASE_URL + '/chat/completions')
+  body = {
+    model: "gpt-3.5-turbo-1106",
+    messages: [
+      {
+        role: "user",
+        content: "You are a helpful assistant designed to talk English with me, make questions, act as a normal person."
+      },
+      {
+        role: "user",
+        content: "#{transcription.body}"
+      }
+    ]
+  }.to_json
+
+  Oj.load(post(url, body).body)
+end
+
+def audio
+  url = URI.parse(BASE_URL + '/audio/speech')
+  body = { model: "tts-1", input: chat["choices"][0]["message"]["content"], voice: "alloy"  }.to_json
+
+  response = post(url, body)
+
+  File.open('speech.mp3', 'wb') { |file| file.write(response.body) }
+end
+
+def execute
+  transcription
+  chat
+  audio
+end
+
+execute
